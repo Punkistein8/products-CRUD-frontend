5598ea5e4f656b5ccaffb071724a33075cd6ba5f
Henry <henry.guerrero0229@utc.edu.ec>
2023-10-01 14:45:03 -0500

    proyecto finalizado

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Producto } from 'src/app/models/Producto';
 import { ProductoService } from 'src/app/services/producto.service';
@@ -13,11 +13,16 @@ import { ProductoService } from 'src/app/services/producto.service';
 export class CrearProductoComponent {
   productoForm: FormGroup;
 
+  titulo = 'Crear Producto';
+
+  id: string | null;
+
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private toastr: ToastrService,
-    private productoService: ProductoService
+    private productoService: ProductoService,
+    private aRouter: ActivatedRoute
   ) {
     this.productoForm = this.fb.group({
       nombre: ['', Validators.required],
@@ -25,6 +30,11 @@ export class CrearProductoComponent {
       ubicacion: ['', Validators.required],
       precio: ['', Validators.required],
     });
+    this.id = this.aRouter.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.esEditar();
   }
 
   agregarProducto() {
@@ -35,12 +45,55 @@ export class CrearProductoComponent {
       precio: this.productoForm.get('precio')?.value,
     };
 
-    console.log(producto);
+    if (this.id !== null) {
+      // Editar producto
+      this.productoService.editarProducto(this.id, producto).subscribe(
+        (res) => {
+          this.toastr.info(
+            'El producto fue actualizado con éxito',
+            'Producto Actualizado'
+          );
+          this.router.navigate(['/']);
+        },
+        (err) => {
+          console.log(err);
+          this.toastr.error('No se pudo actualizar el producto', 'Error');
+        }
+      );
+    } else {
+      // Guardar producto
+      this.productoService.guardarProducto(producto).subscribe(
+        (res) => {
+          console.log(res);
+          this.toastr.success(
+            'Producto registrado con éxito!',
+            'Producto Registrado'
+          );
+          this.router.navigate(['/']);
+        },
+        (err) => {
+          console.log(err);
+          this.toastr.error(
+            'No se pudo registrar el producto',
+            'Error al registrar'
+          );
+          this.productoForm.reset();
+        }
+      );
+    }
+  }
 
-    this.toastr.success(
-      'Producto registrado con éxito!',
-      'Producto Registrado'
-    );
-    this.router.navigate(['/']);
+  esEditar() {
+    if (this.id !== null) {
+      this.titulo = 'Editar Producto';
+      this.productoService.obtenerProducto(this.id).subscribe((data) => {
+        this.productoForm.setValue({
+          nombre: data.nombre,
+          categoria: data.categoria,
+          ubicacion: data.ubicacion,
+          precio: data.precio,
+        });
+      });
+    }
   }
 }
